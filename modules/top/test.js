ddb20b84c9efafca543e46cc30e7e13ba61cfb31
Ludwig Magnusson <ludwig@mediatool.com>
2025-01-20 09:06:09 +0100

    setup workpace with needed modules

---
+++ modules/top/test.js
@@ -0,0 +1,4 @@
+import assert from 'node:assert'
+import { core } from '@nxcache/core'
+
+assert.equal('CORE', core)
---
+++ tools/generator/bin/generate.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const name = process.argv[2]
+
+process.stdout.write(`export const ${name} = '${name.toUpperCase()}'`)
